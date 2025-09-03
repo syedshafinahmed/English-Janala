0052077abd437e35eeca649c2296955714feefce
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-04 01:57:41 +0600

    adjusted navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        siliguri: ['Hind Siliguri', 'sans-serif']
-      },
-    },
-  },
-}
+// module.exports = {
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         poppins: ['Poppins', 'sans-serif'],
+//         siliguri: ['Hind Siliguri', 'sans-serif']
+//       },
+//     },
+//   },
+// }
