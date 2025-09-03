5e817322879bc37ba28946f3c89fa918102d3f38
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-04 00:24:02 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        siliguri: ['Hind Siliguri', 'sans-serif']
+      },
+    },
+  },
+}
