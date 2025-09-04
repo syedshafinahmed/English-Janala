a9c7edd0f6797179520bcb7b0f079ba1387e1aac
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-04 13:03:47 +0600

    added whole word section

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ const displayLevelWord = (words) =>{
         console.log(word);
         const card = document.createElement("div");
         card.innerHTML = `
-        <p> Cat </p>`;
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h1 class="font-bold text-2xl">${word.word}</h1>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <p class="font-bangla text-xl font-medium">"${word.meaning} / ${word.pronunciation}"</p>
+            <div class="flex justify-between items-center">
+                <button class="bg-[#1A91FF10] btn rounded-md hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="bg-[#1A91FF10] btn rounded-md hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-low"></i></button>
+            </div>
+        </div>`;
         wordContainer.append(card);
     })
 }
