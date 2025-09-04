2cb36f8d5f9c3184da0aec0b3efa1ef52259ccac
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-04 12:43:19 +0600

    added dummy word section

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const loadLessons = () => {
         .then(res => res.json())
         .then(json => displayLessons(json.data));
 };
+const loadLevelWord = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data))
+};
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+    words.forEach (word =>{
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <p> Cat </p>`;
+        wordContainer.append(card);
+    })
+}
 const displayLessons = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
     for (let lesson of lessons) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
         levelContainer.append(btnDiv);
     }
 };
